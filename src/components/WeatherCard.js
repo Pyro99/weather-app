38479e7d0b61a59736cbbbd8ac54d1039be9bb08
Utkarsh Gangurde <utkarshgunner10@gmail.com>
2024-02-25 18:23:45 +0530

    fix: removed  console.log

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const WeatherCard = ({ weather }) => {
-    console.log(weather);
   const { name, main, weather: [current], wind, dt } = weather;
 
   const iconUrl = `https://openweathermap.org/img/wn/${current.icon}@2x.png`;
